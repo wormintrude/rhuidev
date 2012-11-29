2055a5b0e0f73f5ade2c0de99912c727fabd27fc
Mariano Vecchioli <vecchioli@redhat.com>
2012-11-29 16:43:20 -0300

    Syntax fix

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -25,7 +25,7 @@ if ON_OPENSHIFT:
             'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': os.environ['OPENSHIFT_APP_NAME'],
             'USER': os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], 
-            'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWD'],
+            'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
             'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],
             'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
         }
