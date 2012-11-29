9157c36d25c852c795242f1078507f0702187cfd
wormintrude <wormintrude@gmail.com>
2012-11-29 12:12:07 -0300

    Modified to work with OpenShift MySQL DB

---
+++ rhui-openshift-db.md
@@ -0,0 +1,4 @@
+Root User: admin
+   Root Password: iJlhrsX4pAYp
+   Database Name: rhuidev
+Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
--- wsgi/openshift/bak.settings.py
+++ wsgi/openshift/bak.settings.py
@@ -22,10 +22,10 @@ if ON_OPENSHIFT:
     # with rhc app cartridge add (see /README in this git repo)
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'rhuidev' # os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),  # Or path to database file if using sqlite3.
-            'USER': 'admin',                      # Not used with sqlite3.
-            'PASSWORD': 'iJlhrsX4pAYp',                  # Not used with sqlite3.
+            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),  # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
