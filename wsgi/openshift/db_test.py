a8aa35482567cf35a9762cf17fd5a72a245b11b7
Mariano Vecchioli <vecchioli@redhat.com>
2012-11-30 10:33:14 -0300

    Style fixes

--- wsgi/openshift/db_test.py
+++ wsgi/openshift/db_test.py
@@ -5,42 +5,37 @@ from django.db import connection
 import os
 
 class db_test(object):
-  def db_connection_test(self):
-    try:
-      import MySQLdb
-      result = "MySQLdb imported succesfully"
-    except:
-      result = "MySQLdb NOT imported"
+	def db_connection_test(self):
+		try:
+			import MySQLdb
+			result = "MySQLdb imported succesfully"
+		except:
+			result = "MySQLdb NOT imported"
 
-    return result
+		return result
 
-  def db_read_test(self):
-    try:
-      # database = mysqldb.connect(host=os.environ['OPENSHIFT_MYSQL_DB_HOST'], user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'], db=os.environ['OPENSHIFT_APP_NAME'])
-      # database = mysqldb.connect(host='localhost', user='admin', passwd='iJlhrsX4pAYp', db='rhuidev')
-      # cursor = database.cursor()
-      cursor = connection.cursor()
-      sql = 'SELECT * FROM rhuidev.usage_data;'
-      cursor.execute(sql)
-      result = cursor.fetchall()
-      ## DEBUG
-      # print sql
-      # print result
-      # print type(result)
-    except:
-      result = "Error reading database"
+	def db_read_test(self):
+  		try:
+			cursor = connection.cursor()
+			sql = 'SELECT * FROM rhuidev.usage_data;'
+			cursor.execute(sql)
+			result = cursor.fetchall()
+			## DEBUG
+			# print sql
+			# print result
+			# print type(result)
+		except:
+			result = "Error reading database"
 
-    return result
+		return result
 
-  def db_write_test(self):
-    try:
-      # database = mysqldb.connect(host=os.environ['OPENSHIFT_MYSQL_DB_HOST'], user=os.environ['OPENSHIFT_MYSQL_DB_USERNAME'], passwd=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'], db=os.environ['OPENSHIFT_APP_NAME'])
-      # database = mysqldb.connect(host='localhost', user='admin', passwd='iJlhrsX4pAYp', db='rhuidev')
-      cursor = connection.cursor()
-      sql = 'CREATE TABLE db_write_test (some_field VARCHAR(4));'
-      result = cursor.execute(sql)
-    except:
-      result = "Error writing to database"
+	def db_write_test(self):
+		try:
+			cursor = connection.cursor()
+			sql = 'CREATE TABLE db_write_test (some_field VARCHAR(4));'
+			result = cursor.execute(sql)
+		except:
+			result = "Error writing to database"
    
-    return result
+		return result
 
